0028480299b017502fe578fe78b4e588c7820661
OzzBabym <ozzozzz@mail.ru>
2021-03-10 15:43:11 +0300

    first

--- server/server.js
+++ server/server.js
@@ -4,10 +4,36 @@ const cors = require('cors')
 // const userRoom = require('./files/schema/userRoom')
 
 const app = express()
-app.use(cors())
+
+let port = process.env.PORT || 9999
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://chatroommess.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use(cors({origin: 'https://chatroommess.herokuapp.com'}))
 
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const io = require('socket.io')(server, {
+    cors: {
+      origin: "https://chatroommess.herokuapp.com",
+      methods: ["GET", "POST"]
+    }
+  })
 
 //=======================================
 // mongoose.connect('mongodb+srv://Ozz:1234@cluster0.smou3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -21,7 +47,7 @@ const io = require('socket.io')(server)
 app.use(express.json())
 
 
-let port = process.env.PORT || 9999
+
 
 const rooms = new Map()
 
--- src/components/socket.js
+++ src/components/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io('wss://chatroommess.herokuapp.com/')
+const socket = io('wss://chatroommess.herokuapp.com/', {transports: ['polling']})
 
 
 export default socket
